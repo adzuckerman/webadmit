49f1085f4dd80433410f61e3112c25d47adf990a
Adam Zuckerman <adam@cloudmineconsulting.com>
2018-10-15 05:47:18 +0000

    trying worker

--- worker.php
+++ worker.php
@@ -1,11 +1,5 @@
 <?php
-$to = "blin@blinkazazi.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: blinkazazi@hotmail.com";
-
-echo mail($to,$subject,$txt,$headers);
-?>
-
-
+while (true) {
+    echo 'Hello world at ' . time() . PHP_EOL;
+    sleep(1);
+}
