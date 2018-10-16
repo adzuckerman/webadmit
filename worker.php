37cc6deb42118a6be7689b800672daf47c709735
vpblin <blin@veritasprep.com>
2018-10-15 22:18:31 -0500

    trying one more

--- worker.php
+++ worker.php
@@ -42,29 +42,43 @@ require 'vendor/autoload.php';
 define('AMQP_DEBUG', true);
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-
 $url = parse_url(getenv('CLOUDAMQP_URL'));
-$conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
 
-$ch = $conn->channel();
+while(true){
+    try{
+        $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
 
-$exchange = 'amq.direct';
-$queue = 'basic_get_queue';
-$ch->queue_declare($queue, false, true, false, false);
-$ch->exchange_declare($exchange, 'direct', true, true, false);
-$ch->queue_bind($queue, $exchange);
+        $ch = $conn->channel();
+
+        $exchange = 'amq.direct';
+        $queue = 'basic_get_queue';
+        $ch->queue_declare($queue, false, true, false, false);
+        $ch->exchange_declare($exchange, 'direct', true, true, false);
+        $ch->queue_bind($queue, $exchange);
 
 
-$retrived_msg = $ch->basic_get($queue);
-echo "received ". $retrived_msg->body . " </br>";
-slow_function($retrived_msg->body);
-$ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
+        $retrived_msg = $ch->basic_get($queue);
+        echo "received ". $retrived_msg->body . " </br>";
+        slow_function($retrived_msg->body);
+        $ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
 
-while (count($ch->callbacks)) {
-    $channel->wait();
+        while (count($ch->callbacks)) {
+            $channel->wait();
+        }
+
+        var_dump($ch->callbacks);
+
+        $ch->close();
+        $conn->close();
+    } catch(AMQPIOException $e) {
+        cleanup_connection($conn);
+        usleep(WAIT_BEFORE_RECONNECT_uS);
+    } catch(\RuntimeException $e) {
+        cleanup_connection($conn);
+        usleep(WAIT_BEFORE_RECONNECT_uS);
+    } catch(\ErrorException $e) {
+        cleanup_connection($conn);
+        usleep(WAIT_BEFORE_RECONNECT_uS);
+    }
+
 }
-
-var_dump($ch->callbacks);
-
-$ch->close();
-$conn->close();
