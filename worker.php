2a7510937d9a70e8d1706dcb5acd0bb0bce61aa9
vpblin <blin@veritasprep.com>
2018-10-17 22:26:36 -0500

    test

--- worker.php
+++ worker.php
@@ -99,7 +99,7 @@ function process_request($request){
             $curl = curl_init();
 
             // Set some options
-            curl_setopt($curl, CURLOPT_URL, 'https://api.webadmit.org/api/v1/user_identities/280464/pdf_manager_zip_files/287986/download');
+            curl_setopt($curl, CURLOPT_URL, 'https://api.webadmit.org/api/v1/user_identities/280464/pdf_manager_zip_files/287988/download');
             curl_setopt($curl, CURLOPT_HTTPHEADER, array('x-api-key:' . $key));
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
