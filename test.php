51baeae54571d24d784008578068911bd0f7cf16
vpblin <blin@veritasprep.com>
2018-10-17 19:45:19 -0500

    var dump for debugging

--- test.php
+++ test.php
@@ -13,14 +13,10 @@ $queue = 'basic_get_queue';
 $ch->queue_declare($queue, false, true, false, false);
 $ch->exchange_declare($exchange, 'direct', true, true, false);
 $ch->queue_bind($queue, $exchange);
-$i = 2;
 
-while($i < 50){
-    $msg_body = $i;
-    $msg = new AMQPMessage($msg_body, array('content_type' => 'application/json', 'delivery_mode' => 2));
+    $msg = new AMQPMessage('{"pdf_manager_batch":{"id":6348,"state":"available","updated_at":"2018-10-18T00:07:13Z","pdf_manager_template":{"id":8153,"name":"Transcripts","href":"\/api\/v1\/user_identities\/280464\/pdf_manager_templates\/8153"},"href":"\/api\/v1\/user_identities\/280464\/pdf_manager_batches\/6348","download_hrefs":["\/api\/v1\/user_identities\/280464\/pdf_manager_zip_files\/287866\/download"]}}', array('content_type' => 'text/plain', 'delivery_mode' => 2));
     $ch->basic_publish($msg, $exchange);
     $i ++;
-}
 
 $ch->close();
 $conn->close();
